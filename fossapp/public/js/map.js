d176d233ef56bc54a8f407e1347a07d948d731ac
Jason Sanford <jasonsanford@gmail.com>
2011-09-02 23:48:05 -0600

    Attempting to use markers instead of map tiles to show FOSS4G venues.

--- fossapp/public/js/map.js
+++ fossapp/public/js/map.js
@@ -46,6 +46,27 @@ bd.overlayMaps = [
 	}
 ];
 
+bd.venues = [
+    {
+        "desc": '<h2>Opening Social</h2><h3>Wynkoop Brewery</h3><p>Come taste some fine Denver microbrews and test your billiards skills against a local pro, <a href="http://www.wynkoop.com/billiards/viper-blog">Melissa "The Viper" Little</a>. We\'ll have the second floor to ourselves. Come help us kick off the conference!</p><p class="more-info"><a href="http://2011.foss4g.org/content/opening-social-wynkoop-brewery">More Info</a></p>',
+        "lat": 39.75356211538687,
+        "lng": -104.99846756458282
+    },{
+        "desc": '<h2>The Conference</h2><h3>Sheraton Denver Downtown</h3><p>The home of the 2011 FOSS4G Conference.</p><p class="more-info"><a href="http://www.sheratondenverdowntown.com/">More Info</a></p>',
+        "lat": 39.74269405515172,
+        "lng": -104.9893319606781,
+        "is_the_hotel_where_all_this_stuff_is_happening": true
+    },{
+        "desc": '<h2>The Big Party</h2><h3>Denver Art Museum</h3><p>The <a href="http://www.denverartmuseum.org/home">Denver Art Museum</a> will be the place to be on Thursday, September 15 for the Big Party. Come explore the <a href="http://expansion.denverartmuseum.org/">Hamilton Building</a> where we\'ll have access to several galleries as well as the gift shop. This is a great event for networking, so don\'t miss it!</p><p class="more-info"></p>',
+        "lat": 39.737496577957,
+        "lng": -104.9897128343582
+    },{
+        "desc": '<h2>Code Sprint</h2><h3>Tivoli Student Union</h3><p>Join us here Saturday, Sept 17 from 9:00am to 6:00pm at the Tivoli Student Union to work on some great open source geo projects. Not a coder? These projects also need people to test, document and give feedback. To sign up, head over to the <a href="http://wiki.osgeo.org/wiki/FOSS4G_2011_Code_Sprint">Code Sprint Wiki</a>.</p><p class="more-info"><a href="http://2011.foss4g.org/content/code-sprint">More Info</a></p>',
+        "lat": 39.745181280550725,
+        "lng": -105.00589728355408
+    }
+];
+
 $(document).ready(function(){
 
 	bd.map = new L.Map("map-container", {
@@ -74,6 +95,20 @@ $(document).ready(function(){
 		}
 		overlayMaps[o.name] = o.layer;
 	});
+	
+	$.each( bd.venues, function( i, o ) {
+	    var icon = L.Icon.extend( {
+	        iconUrl: o.is_the_hotel_where_all_this_stuff_is_happening ? "img/marker/foss4g-large.png" : "img/marker/foss4g-large.png",
+	        iconSize: o.is_the_hotel_where_all_this_stuff_is_happening ? new L.Point( 40, 24 ) : new L.Point( 34, 21 ),
+	        iconAnchor: o.is_the_hotel_where_all_this_stuff_is_happening ? new L.Point( 21, 13 ) : new L.Point( 17, 11 ),
+	        popupAnchor: o.is_the_hotel_where_all_this_stuff_is_happening ? new L.Point( 21, 13 ) : new L.Point( 17, 11 )
+	    } );
+	    o.marker = new L.Marker( new L.LatLng( o.lat, o.lng ), {
+	        icon: icon
+	    } );
+	    o.marker.bindPopup( o.desc );
+	    mw.map.addLayer( o.marker )
+	} );
 	
 	if (window.handleMapClick){
 	    bd.map.on("click", handleMapClick);
